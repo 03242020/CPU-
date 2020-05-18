59df9da723a97a1f21997ecbb6b9be759e1297c3
03242020 <62064207+03242020@users.noreply.github.com>
2020-05-18 19:32:53 +0900

    Add files via upload

---
+++ MyJanken/MyJanken/ViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    //じゃんけん(数字)
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+   // 新しいじゃんけんの結果を一時的に格納する変数を設ける
+        var newAnswerNumber = 0
+        
+        /* ランダムに結果を出すが、前回の結果と異なる場合のみ採用
+             repeatとは繰り返しを意味する */
+        repeat {
+            
+            /* 0,1,2の数値をランダムに算出(乱数)
+            arc4random_uniform()の戻り値はUInt32だがswiftの標準的な整数型Intにキャストする */
+            newAnswerNumber = Int(arc4random_uniform(3))
+            
+            /* 前回と同じ結果の時は再度ランダムに数値を出す
+            異なる結果の時は、repeatを抜ける */
+        } while answerNumber == newAnswerNumber
+        
+        // 新しいじゃんけんの結果を格納
+        answerNumber  = newAnswerNumber
+        
+        if answerNumber == 0 {
+            // グー
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named: "gu")
+        } else if answerNumber == 1 {
+            // チョキ
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named: "choki")
+        } else if answerNumber == 2{
+        answerLabel.text = "パー"
+        answerImageView.image = UIImage(named:"pa")
+    }
+}
+}
